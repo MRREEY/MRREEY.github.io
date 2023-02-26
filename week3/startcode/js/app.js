303ec5363effaa291e2f96a7b18419efddcdfd13
MRREEY <schraagenreymark@gmail.com>
2023-02-26 22:45:28 +0100

    Huiswerk week 3

--- week3/startcode/js/app.js
+++ week3/startcode/js/app.js
@@ -3,14 +3,51 @@ const label = document.getElementById("label");
 
 const labelOneBtn = document.querySelector("#labelOne");
 const labelTwoBtn = document.querySelector("#labelTwo");
-const labelThreeBtn = document.querySelector("#labelThree");
+const voorspel = document.querySelector("#voorspel");
 const trainbtn = document.querySelector("#train");
 
-labelOneBtn.addEventListener("click", () => console.log("button 1"));
-labelTwoBtn.addEventListener("click", () => console.log("button 2"));
-labelThreeBtn.addEventListener("click", () => console.log("button 3"));
+const amountOfSteenimages = document.getElementById("amountOfSteenImages");
+const amountOfPapierimages = document.getElementById("amountOfPapierImages");
 
-trainbtn.addEventListener("click", () => console.log("train"));
+const train = document.getElementById("train");
+const save = document.getElementById("save");
+const loss = document.getElementById("loss");
+
+// Extract the already learned features from MobileNet
+const featureExtractor = ml5.featureExtractor('MobileNet', modelLoaded);
+
+// When the model is loaded
+function modelLoaded() {
+  console.log('Model Loaded!');
+  classifier.load('./my_model/model.json', customModelReady);
+  speak(label.innerHTML)
+}
+
+function customModelReady() {
+  console.log('Custom model is ready')
+}
+
+// Create a new classifier using those features and with a video element
+const classifier = featureExtractor.classification(video, videoReady);
+
+// Triggers when the video is ready
+function videoReady() {
+  console.log('De video is klaar!');
+}
+
+//testing speech
+let synth = window.speechSynthesis
+
+function speak(text) {
+    if (synth.speaking) {
+        console.log('Blijft praten....')
+        return
+    }
+    if (text !== '') {
+        let utterThis = new SpeechSynthesisUtterance(text)
+        synth.speak(utterThis)
+    }
+}
 
 if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices
@@ -19,8 +56,51 @@ if (navigator.mediaDevices.getUserMedia) {
             video.srcObject = stream;
         })
         .catch((err) => {
-            console.log("Something went wrong!");
+            console.log("Camera kapot...");
         });
 }
 
-label.innerText = "Ready when you are!";
+//maakt foto's die getraind kunnen worden en geeft het aantal foto's met 
+//naam
+labelOneBtn.onclick = function() {
+    classifier.addImage(video, "steen");
+    amountOfSteenimages.innerText = Number(amountOfSteenimages.innerText) + 1;
+  };
+
+labelTwoBtn.onclick = function() {
+    classifier.addImage(video, "papier");
+    amountOfPapierimages.innerText = Number(amountOfPapierimages.innerText) + 1;
+  };
+
+  train.onclick = function() {
+    classifier.train(function(lossValue) {
+      if (lossValue) {
+        totalLoss = lossValue;
+        loss.innerHTML = `Loss: ${totalLoss}`;
+      } else {
+        loss.innerHTML = `Done Training! Final Loss: ${totalLoss}`;
+      } 
+    });
+  };
+
+  save.onclick = function() {
+    classifier.save();
+  };
+
+voorspel.onclick = function() {
+  featureExtractor.classify(gotVoorspelling);
+}
+
+// Show the results
+function gotVoorspelling(err, voorspellingen) {
+  // Display any error
+  if (err) {
+    console.error(err);
+  }
+  if (voorspellingen && voorspellingen[0]) {
+    voorspel.innerText = voorspellingen[0].label;
+    confidence.innerText = voorspellingen[0].confidence;
+    classifier.classify(gotVoorspelling);
+    speak("ik denk dat het:" + voorspel.innerHTML + "is")
+  }
+}
